c9a2cebd8f258bb5ee716822751a1aae2b1d0e98
Max Sky <max940213@live.com>
2022-09-28 19:58:30 +0800

    调整与 PHPUnit 兼容性

--- src/loader.php
+++ src/loader.php
@@ -13,15 +13,30 @@
 // | github 仓库地址 ：https://github.com/zoujingli/ThinkLibrary
 // +----------------------------------------------------------------------
 
-use think\Console;
-use think\facade\Env;
-use think\facade\Lang;
-use think\facade\Middleware;
-use think\Request;
+// 注册系统常用指令
+if (class_exists('think\Console')) {
+    think\Console::addDefaultCommands([
+        // 注册清理无效会话
+        '\\library\\command\\Sess',
+        // 注册系统任务指令
+        '\\library\\queue\\WorkQueue',
+        '\\library\\queue\\StopQueue',
+        '\\library\\queue\\StateQueue',
+        '\\library\\queue\\StartQueue',
+        '\\library\\queue\\QueryQueue',
+        '\\library\\queue\\ListenQueue',
+        // 注册系统更新指令
+        '\\library\\command\\sync\\Admin',
+        '\\library\\command\\sync\\Plugs',
+        '\\library\\command\\sync\\Config',
+        '\\library\\command\\sync\\Wechat',
+        '\\library\\command\\sync\\Service',
+    ]);
+}
 
 if (PHP_SAPI !== 'cli') {
     // 注册跨域中间键
-    Middleware::add(function (Request $request, \Closure $next, $header = []) {
+    think\facade\Middleware::add(function (\think\Request $request, \Closure $next, $header = []) {
         if (($origin = $request->header('origin', '*')) !== '*') {
             $header['Access-Control-Allow-Origin'] = $origin;
             $header['Access-Control-Allow-Methods'] = 'GET,POST,PATCH,PUT,DELETE';
@@ -36,34 +51,14 @@ if (PHP_SAPI !== 'cli') {
     });
 }
 
-// 注册系统常用指令
-if (class_exists('think\Console')) {
-    Console::addDefaultCommands([
-        // 注册清理无效会话
-        'library\command\Sess',
-        // 注册系统任务指令
-        'library\queue\WorkQueue',
-        'library\queue\StopQueue',
-        'library\queue\StateQueue',
-        'library\queue\StartQueue',
-        'library\queue\QueryQueue',
-        'library\queue\ListenQueue',
-        // 注册系统更新指令
-        'library\command\sync\Admin',
-        'library\command\sync\Plugs',
-        'library\command\sync\Config',
-        'library\command\sync\Wechat',
-        'library\command\sync\Service',
-    ]);
-}
-
-// 加载对应的语言包
-Lang::load(__DIR__ . '/lang/zh-cn.php', 'zh-cn');
-Lang::load(__DIR__ . '/lang/en-us.php', 'en-us');
-
 // 动态加载模块配置
 if (function_exists('think\__include_file')) {
-    $root = rtrim(str_replace('\\', '/', Env::get('app_path')), '/');
+    // 加载对应的语言包
+    think\facade\Lang::load(__DIR__ . '/lang/zh-cn.php', 'zh-cn');
+    think\facade\Lang::load(__DIR__ . '/lang/en-us.php', 'en-us');
+
+    $root = rtrim(str_replace('\\', '/', think\facade\Env::get('app_path')), '/');
+
     foreach (glob("{$root}/*/sys.php") as $file) {
         \think\__include_file($file);
     }
